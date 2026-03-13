1b791e8d87e9acb045ea65c3a3dfad9da6796d0b
SyedJunaidAli1 <syedjunaidali790@gmail.com>
2026-03-13 15:47:17 +0530

    code clean up

--- app/components/data/steps.ts
+++ app/components/data/steps.ts
@@ -2,37 +2,21 @@ export const steps = [
   {
     id: 1,
     title: "Upload Strategy",
-    description: "Upload your trading strategy to the platform."
+    description: "Upload your trading strategy to the platform.",
   },
   {
     id: 2,
     title: "AI Analysis",
-    description: "Our system analyzes performance and risk."
+    description: "Our system analyzes performance and risk.",
   },
   {
     id: 3,
     title: "Optimization",
-    description: "Strategies are optimized for better performance."
+    description: "Strategies are optimized for better performance.",
   },
   {
     id: 4,
     title: "Execution",
-    description: "Deploy your strategy in live markets."
+    description: "Deploy your strategy in live markets.",
   },
-  {
-    id: 5,
-    title: "dasd",
-    description: "dasda"
-  },
-  {
-    id: 6,
-    title: "",
-    description: ""
-  },
-  {
-    id: 7,
-    title: "",
-    description: ""
-  },
-  
-]
+];
